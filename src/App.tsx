88ddd46f81a2dc8ebca31fc863ed7a315c3ad201
pedro henrique <pedro.henrike2812@gmail.com>
2023-02-12 12:39:00 -0300

    iniciando clone do twitter

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>twitter</h1>
+  )
+}
+
+export default App
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,11 @@
+export function Layout () {
+    return (
+        <Container>
+            <Wrapper>
+                <MenuBar/>
+                <Main/>
+                <SideBar/>
+            </Wrapper>
+        </Container>
+    )
+}
